0500638ba9a7c2c2403ca333433fdf357ba3431a
Javier <ajmontigel@gmail.com>
2021-04-25 21:57:43 -0300

    .

---
+++ tiendapome.backend/tiendapome.Entidades/TipoMovimientoStock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace tiendapome.Entidades
+{
+    [JsonObject]
+    public class TipoMovimientoStock : EntidadBaseParametrica
+    {
+        [JsonProperty("Tipo")]
+        public virtual int Tipo { get; set; }
+
+        public TipoMovimientoStock() { }
+    }
+}
